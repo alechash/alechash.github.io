7aed1a0593727497f02080fd27b63d0a2d9a9004
Jude Wilson <jude@alecw.net>
2021-02-28 21:39:04 -0500

    added 404 page and light mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,34 @@ document.getElementById("pgp_button").addEventListener('click', function () {
 })
 
 document.getElementById('pageHeight').innerText = `this page is ${document.body.scrollHeight} pixels tall`
+
+var pullChain = document.getElementById("pull-chain");
+var css = document.getElementById("css");
+var graph = document.getElementById("graph");
+var toggled = false
+
+pullChain.addEventListener("click", function () {
+    startTimer();
+}, false);
+
+function startTimer() {
+    pullChain.classList.toggle("pulled");
+    setTimeout(stopTimer, 500);
+}
+
+function stopTimer() {
+    toggleLights();
+    pullChain.classList.toggle("pulled");
+}
+
+function toggleLights() {
+    if (toggled == true) {
+        css.setAttribute('href', '/assets/css/index.css')
+        graph.setAttribute('href', '/assets/css/dark_graph.css')
+        toggled = false
+    } else {
+        css.setAttribute('href', '/assets/css/light.css')
+        graph.setAttribute('href', '/assets/css/light_graph.css')
+        toggled = true
+    }
+}
